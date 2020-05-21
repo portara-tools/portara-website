dc6d51aab252a136eefda72cc6d1ee6cc03cef8d
Cary Chan <caryLchan@gmail.com>
2020-05-21 11:54:39 -0700

    React Spring components refactored

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import LandingPage from './components/LandingPage'
 
 function App() {
   return (
     <div className="App">
-      Running
+      <LandingPage />
     </div>
   );
 }

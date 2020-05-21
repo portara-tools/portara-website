7e61c842e621ef1f4b6efd5cadb1025a99c58865
Steve Frend <stevefrend1@gmail.com>
2020-05-21 12:00:58 -0700

    Refactoring more UI to React

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+// import { useTheme} from '@material-ui/core/styles';
+import LandingPage from './components/LandingPage';
 
 function App() {
   return (
-    <div className="App">
-      Running
-    </div>
+
+      <div style={{backgroundColor: "black"}}>
+        <LandingPage />
+      </div>
+
   );
 }
 
--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,43 +1,32 @@
 import * as React from 'react';
-import Link from 'next/link';
-import Head from 'next/head';
 import { AppBar, Link as LinkM, Button, Box } from '@material-ui/core';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import { makeStyles } from '@material-ui/core/styles';
 
-type Props = {
-  title?: string;
-};
 
-const Layout: React.FunctionComponent<Props> = (props) => {
-  const { children, title = 'This is the default title' } = props;
+const Navbar: React.FunctionComponent = (props) => {
   const classes = useStyles(props);
   return (
     <>
-      <Head>
-        <title>{title}</title>
-        <meta charSet='utf-8' />
-        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
-      </Head>
 
       <Box className={classes.container}>
         <AppBar position='static' className={classes.appBar}>
           <ul className={classes.linksUL}>
-            <Link href='/'>
+
               <LinkM variant='overline' underline='none' className={classes.linkStyle}>
                 HOME
               </LinkM>
-            </Link>
-            <Link href='/documentation'>
+
+
               <LinkM variant='overline' underline='none' className={classes.linkStyle}>
                 DOCUMENTATION
               </LinkM>
-            </Link>
-            <Link href='/about'>
+
+
               <LinkM variant='overline' underline='none' className={classes.linkStyle}>
                 ABOUT
               </LinkM>
-            </Link>
+
           </ul>
           <ul className={classes.loginButton}>
             <LinkM variant='overline' underline='none'>
@@ -49,12 +38,11 @@ const Layout: React.FunctionComponent<Props> = (props) => {
           </ul>
         </AppBar>
       </Box>
-      {children}
     </>
   );
 };
 
-export default Layout;
+export default Navbar;
 
 const useStyles = makeStyles(() => ({
   appBar: {

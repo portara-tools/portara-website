d5c0d3676ab4d236768bb89da042f5640862d2f0
Steve Frend <stevefrend1@gmail.com>
2020-05-21 19:52:55 -0700

    Heroku test 1

--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,7 @@
-const { ApolloServer, gql } = require('apollo-server');
-// import { typeDefs } from './api/schema';
-// import { resolvers } from './api/resolvers';
-// import {  } from './api/models';
+// const { ApolloServer, gql } = require('apollo-server');
+import { ApolloServer, gql } from 'apollo-server-express'
+const express = require('express');
+const path = require('path');
 
 const typeDefs = gql`
   type Query {
@@ -55,13 +55,25 @@ const resolvers = {
   },
 };
 
+const PORT = process.env.PORT || 4000;
+
+const app = express();
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  // context: ({ req, res }) => ({ req, res }),
 });
 
-server.listen({ port: 4000 }, () => {
-  console.log(`Server running @ PORT 4000`);
+app.use(express.static('public'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'))  
+})
+
+server.applyMiddleware({
+  app,
 });
+
+app.listen({port: PORT || 4000}, () => {
+  console.log(`Listening @ ${PORT}`)
+})

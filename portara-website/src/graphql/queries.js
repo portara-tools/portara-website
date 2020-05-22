0c72f0b5042b938c165f0ababa905e3a6cd6f537
Alexander Infante <alexinfante17@gmail.com>
2020-05-22 00:39:49 -0700

    AWS Amplify and AppSync restructuring. Reconfiguring application to deploy with serverless and GraphQL subscriptions.

---
+++ portara-website/src/graphql/queries.js
@@ -0,0 +1,96 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const getUser = /* GraphQL */ `
+  query GetUser($id: ID!) {
+    getUser(id: $id) {
+      id
+      username
+      rateLimiters {
+        items {
+          id
+          projectName
+          limit
+          per
+          throttle
+          userId
+          createdAt
+          updatedAt
+        }
+        nextToken
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const listUsers = /* GraphQL */ `
+  query ListUsers(
+    $filter: ModelUserFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listUsers(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        username
+        rateLimiters {
+          nextToken
+        }
+        createdAt
+        updatedAt
+      }
+      nextToken
+    }
+  }
+`;
+export const getRateLimiter = /* GraphQL */ `
+  query GetRateLimiter($id: ID!) {
+    getRateLimiter(id: $id) {
+      id
+      projectName
+      limit
+      per
+      throttle
+      userId
+      user {
+        id
+        username
+        rateLimiters {
+          nextToken
+        }
+        createdAt
+        updatedAt
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const listRateLimiters = /* GraphQL */ `
+  query ListRateLimiters(
+    $filter: ModelRateLimiterFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listRateLimiters(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        projectName
+        limit
+        per
+        throttle
+        userId
+        user {
+          id
+          username
+          createdAt
+          updatedAt
+        }
+        createdAt
+        updatedAt
+      }
+      nextToken
+    }
+  }
+`;
---
+++ portara-website/src/graphql/subscriptions.js
@@ -0,0 +1,141 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateUser = /* GraphQL */ `
+  subscription OnCreateUser {
+    onCreateUser {
+      id
+      username
+      rateLimiters {
+        items {
+          id
+          projectName
+          limit
+          per
+          throttle
+          userId
+          createdAt
+          updatedAt
+        }
+        nextToken
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateUser = /* GraphQL */ `
+  subscription OnUpdateUser {
+    onUpdateUser {
+      id
+      username
+      rateLimiters {
+        items {
+          id
+          projectName
+          limit
+          per
+          throttle
+          userId
+          createdAt
+          updatedAt
+        }
+        nextToken
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteUser = /* GraphQL */ `
+  subscription OnDeleteUser {
+    onDeleteUser {
+      id
+      username
+      rateLimiters {
+        items {
+          id
+          projectName
+          limit
+          per
+          throttle
+          userId
+          createdAt
+          updatedAt
+        }
+        nextToken
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onCreateRateLimiter = /* GraphQL */ `
+  subscription OnCreateRateLimiter {
+    onCreateRateLimiter {
+      id
+      projectName
+      limit
+      per
+      throttle
+      userId
+      user {
+        id
+        username
+        rateLimiters {
+          nextToken
+        }
+        createdAt
+        updatedAt
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateRateLimiter = /* GraphQL */ `
+  subscription OnUpdateRateLimiter {
+    onUpdateRateLimiter {
+      id
+      projectName
+      limit
+      per
+      throttle
+      userId
+      user {
+        id
+        username
+        rateLimiters {
+          nextToken
+        }
+        createdAt
+        updatedAt
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteRateLimiter = /* GraphQL */ `
+  subscription OnDeleteRateLimiter {
+    onDeleteRateLimiter {
+      id
+      projectName
+      limit
+      per
+      throttle
+      userId
+      user {
+        id
+        username
+        rateLimiters {
+          nextToken
+        }
+        createdAt
+        updatedAt
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;

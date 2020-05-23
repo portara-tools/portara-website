b8f0283d1d71188a932c64b6c6c027410b0da890
Cary Chan <caryLchan@gmail.com>
2020-05-23 16:02:42 -0700

    Added Mongo DB from mLab through Heroku

--- server/server.ts
+++ server/server.ts
@@ -10,7 +10,7 @@ const mongoose = require('mongoose')
 
 // Mongo Connection
 
-const URI = `mongodb+srv://stevefrend:_--wC2-!TqUtsTJx@solo-vue4b.mongodb.net/test?retryWrites=true&w=majority`;
+const URI = `mongodb://heroku_wcgfs261:n1g8tpuc2nmb8bj8d8jt24hd8v@ds137263.mlab.com:37263/heroku_wcgfs261`;
 mongoose.connect(URI, { useUnifiedTopology: true, useNewUrlParser: true }, () =>
   console.log('connected to MongoDB')
 );
@@ -20,19 +20,13 @@ const userSchema = new mongoose.Schema({
   portara: [{ name: String, limit: String, per: String, throttle: String }],
 });
 
-// const portaraSchema = new mongoose.Schema({
-//     limit: String || Number,
-//   per: String || Number,
-//   throttle: String || Number
-// })
 
 const User = mongoose.model('portaraUsers', userSchema);
-
 
 // typeDefs
 const typeDefs = gql`
   type Query {
-    portaraSettings(userID: String!, limit: ID!, per: ID!, throttle: ID!): PortaraSetting!
+    test: String!
   }
   type Subscription {
     portaraSettings(userID: String!): PortaraSetting!
@@ -57,51 +51,50 @@ const typeDefs = gql`
 const resolvers = {
   Subscription: {
     portaraSettings: {
-      // also need args here to establish user ID as channel
       subscribe(_, { userID }) {
-        console.log(userID)
-        // Inside here, we reach into the database to see if the user exists. If it doesn't, userID = 'default'. If it does, userID = their email/ip/whatever
+        // console.log(userID)
         return pubsub.asyncIterator(userID)
       }
     }
   },
   Query: {
-    portaraSettings: (_, { userID, limit, per, throttle }) => {
-      pubsub.publish(userID, { portaraSettings: { name, limit, per, throttle } })
-      return { name, limit, per, throttle }
-    },
+    test: () => "Test success"
+
   },
   Mutation: {
     changeSetting: async (_, { userID, name, limit, per, throttle }) => {
-      const data = await User.findById(userID)
-      const arr = [...data.portara]
-      const Field = {
-        name,
-        limit,
-        per,
-        throttle
-      };
-
-      let found = false
-      for (let field of arr) {
-        if (field.name === name) {
-          field.limit = limit;
-          field.per = per;
-          field.throttle = throttle
-          found = true
-          break
+      try {
+        const data = await User.findById(userID)
+        const arr = [...data.portara]
+        const Field = {
+          name,
+          limit,
+          per,
+          throttle
+        };
+        let found = false
+        for (let field of arr) {
+          if (field.name === name) {
+            field.limit = limit;
+            field.per = per;
+            field.throttle = throttle
+            found = true
+            break
+          }
         }
-      }
-      if (!found) {
-        arr.push(Field)
+        if (!found) {
+          arr.push(Field)
+        }
+        await User.findOneAndUpdate(userID, { $set: { portara: arr } }, { upsert: true })
 
+        // const doodoo = await User.findById(userID)
+        // console.log('steves data', doodoo)
+        pubsub.publish(userID, { portaraSettings: { name, limit, per, throttle } })
+        return { userID, name, limit, per, throttle }
+      } catch (error) {
+        return error;
       }
-      await User.findOneAndUpdate(userID, { $set: { portara: arr } }, { upsert: true })
 
-      // const doodoo = await User.findById(userID)
-      // console.log('steves data', doodoo)
-      pubsub.publish(userID, { portaraSettings: { name, limit, per, throttle } })
-      return { userID, name, limit, per, throttle }
     }
   }
 };

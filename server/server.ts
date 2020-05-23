d26939a04988c2fa8c845a7842dec81fe0e1f233
Cary Chan <caryLchan@gmail.com>
2020-05-23 11:20:27 -0700

    Unique name feature added to identify the user of the portara tool

--- server/server.ts
+++ server/server.ts
@@ -5,9 +5,27 @@ const express = require('express');
 const path = require('path');
 const http = require('http');
 const pubsub = new PubSub();
+const mongoose = require('mongoose')
 
 
+// Mongo Connection
 
+const URI = `mongodb+srv://stevefrend:_--wC2-!TqUtsTJx@solo-vue4b.mongodb.net/test?retryWrites=true&w=majority`;
+mongoose.connect(URI, { useUnifiedTopology: true, useNewUrlParser: true }, () =>
+  console.log('connected to MongoDB')
+);
+
+const userSchema = new mongoose.Schema({
+  userID: String,
+  limit: String || Number,
+  per: String || Number,
+  throttle: String || Number
+});
+
+const User = mongoose.model('portaraUsers', userSchema);
+
+
+// typeDefs
 const typeDefs = gql`
   type Query {
     portaraSettings(userID: String!, limit: ID!, per: ID!, throttle: ID!): Settings!
@@ -23,13 +41,14 @@ const typeDefs = gql`
   }
 `;
 
+// resolvers
 const resolvers = {
   Subscription: {
     portaraSettings: {
       // also need args here to establish user ID as channel
       subscribe(_, { userID }) {
         console.log(userID)
-
+        // Inside here, we reach into the database to see if the user exists. If it doesn't, userID = 'default'. If it does, userID = their email/ip/whatever
         return pubsub.asyncIterator(userID)
       }
     }

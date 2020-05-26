68ad2b04375a7ef873c98e420e4fe6f3f2896891
Steve Frend <stevefrend1@gmail.com>
2020-05-26 14:43:33 -0700

    Deployment test CLI 1

---
+++ server/public/precache-manifest.67fcbeef07736fba8707a4f8467bb148.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b484cd9934278ec0160366666cad8dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1f23c9968ed4cce2f32",
+    "url": "/static/js/2.f1ca806f.chunk.js"
+  },
+  {
+    "revision": "799f3d5c27694f0f3c92bbc8286bf099",
+    "url": "/static/js/2.f1ca806f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b90838bd3fbd934e74cc",
+    "url": "/static/js/main.6377f105.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);

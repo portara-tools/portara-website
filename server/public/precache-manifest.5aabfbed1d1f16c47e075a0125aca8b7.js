b80e2674d48c06bcbd73ea08ae3bf409de91f0b6
Steve Frend <stevefrend1@gmail.com>
2020-05-26 12:28:21 -0700

    built pre-deploy

---
+++ server/public/precache-manifest.5aabfbed1d1f16c47e075a0125aca8b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32625dafc3d760eed5ba009026eeba50",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1f23c9968ed4cce2f32",
+    "url": "/static/js/2.f1ca806f.chunk.js"
+  },
+  {
+    "revision": "799f3d5c27694f0f3c92bbc8286bf099",
+    "url": "/static/js/2.f1ca806f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "005fdb51cfad5f863cd1",
+    "url": "/static/js/main.d19c9bc1.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);

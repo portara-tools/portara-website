deffcc4660c733c06b4ec5cbeb7543835a85c805
Cary Chan <caryLchan@gmail.com>
2020-05-27 19:47:46 -0700

    Build public folder added

---
+++ server/public/precache-manifest.4ee4c24ae8f778b41bb54c344dfb7f0e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dcf2052c8e32175574b574833c4edf3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed8adbabaec062bfdf0b",
+    "url": "/static/js/2.63fcaeab.chunk.js"
+  },
+  {
+    "revision": "dcad5f039dd113df8b8a8b3b8d230abf",
+    "url": "/static/js/2.63fcaeab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a24e507b77fbd22b93d",
+    "url": "/static/js/main.7381e8d4.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);

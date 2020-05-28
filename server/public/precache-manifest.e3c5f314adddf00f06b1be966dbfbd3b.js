598aab5820613cd68e61b183c538f31656a441b3
Steve Frend <stevefrend1@gmail.com>
2020-05-28 11:21:25 -0700

    Pre-deploy

---
+++ server/public/precache-manifest.e3c5f314adddf00f06b1be966dbfbd3b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6fa15c4753c52d9692bda6d04798b9d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6c99726602efcd5b11d",
+    "url": "/static/js/2.ebe358b2.chunk.js"
+  },
+  {
+    "revision": "bcaa9f72b0c642d09d382b3c91aa4814",
+    "url": "/static/js/2.ebe358b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64b242b7ebe94264ac23",
+    "url": "/static/js/main.c78c358b.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
